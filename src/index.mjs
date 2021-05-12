7b1f33d5cad371fb67060872cbd9448c94177ed5
Keylane <keylanessilva07@gmail.com>
2021-05-12 09:14:47 -0300

    chamadas principais

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,15 @@
 import Controller from "./controller.mjs";
 import View from "./view.mjs";
+import Media from "./ultil/media.mjs";
+import Recorder from "./ultil/recorder.mjs";
 
 const view = new View();
+const media = new Media();
+const recorder = new Recorder(); 
 
 Controller.initialize({
-    view
+    view,
+    media,
+    recorder
+
 })
