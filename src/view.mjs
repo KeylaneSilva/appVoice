936612500c057b59922e88e9facf70e4b7eda6df
Keylane <keylanessilva07@gmail.com>
2021-05-12 09:15:22 -0300

    view da aplicação

--- src/view.mjs
+++ src/view.mjs
@@ -9,10 +9,34 @@ export default class View{
     onRecordClick(command){
         return () => {
             command()
+            this.toggleAudioElement({ visible: false})
+        }
+    }
+
+    onStopRecordClick(command){
+        return () => {
+            command()
         }
     }
 
     configureStartRecordingButton(command){
         this.btnStart.addEventListener('click', this.onRecordClick(command));
+    }
+
+    configureStopRecordingButton(command){
+        this.btnStop.addEventListener('click', this.onStopRecordClick(command));
+    }
+
+    toggleAudioElement({ visible }) {
+        const classList = this.audioElement.classList
+        visible ? classList.remove('hidden') : classList.add('hidden')
+    }
+
+    playAudio(url){
+        const audio = this.audioElement
+        audio.src = url
+        audio.muted = false
+        this.toggleAudioElement({ visible: true})
+        audio.addEventListener("loadedmetadata", _=> audio.play())
     }
 }
